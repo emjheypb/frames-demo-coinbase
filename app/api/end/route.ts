b3b45072bbd68865ea1db25d2e788818ae705cb3
EmJhey <emjheypb@gmail.com>
2024-07-08 16:09:17 -0400

    initial commit

---
+++ app/api/end/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest): Promise<Response> => {
+  const data = await req.json();
+  const buttonId = data.untrustedData.buttonIndex;
+
+  const path = buttonId === 1 ? "gotogithub" : "gotoyoutube";
+
+  const headers = new Headers();
+  headers.set("Location", `${process.env.VERCEL_URL}/`);
+
+  const response = NextResponse.redirect(`${process.env.VERCEL_URL}/${path}`, {
+    headers: headers,
+    status: 302,
+  });
+
+  return response;
+};
+
+export const dynamic = "force-dynamic";
---
+++ app/api/frame/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const getResponse = async (req: NextRequest): Promise<NextResponse> => {
+  const searchParams = req.nextUrl.searchParams;
+  const id: any = searchParams.get("id");
+  const idAsNumber = parseInt(id);
+
+  if (idAsNumber === 3) {
+    return new NextResponse(
+      `<!DOCTYPE html><html>
+        <head>
+        <title>Frame 7</title>
+        <meta property="fc:frame" content="vNext"/>
+        <meta property="fc:frame:image" content="${process.env.VERCEL_URL}/next.svg"/>
+        <meta property="fc:frame:button:1" content="GitHub"/>
+        <meta property="fc:frame:button:1:action" content="post_redirect"/>
+        <meta property="fc:frame:button:2" content="YouTube Link"/>
+        <meta property="fc:frame:button:2:action" content="link"/>
+        <meta property="fc:frame:button:2:target" content="https://youtube.com"/>
+        <meta property="fc:frame:post_url" content="${process.env.VERCEL_URL}/api/end"/>
+        </head>
+        </html>`
+    );
+  } else {
+    return new NextResponse(
+      `<!DOCTYPE html><html>
+          <head>
+          <title>Frame ${id}</title>
+          <meta property="fc:frame" content="vNext"/>
+          <meta property="fc:frame:image" content="${
+            process.env.VERCEL_URL
+          }/vercel.svg"/>
+          ${
+            idAsNumber > 1 && `<meta property="fc:frame:button:1" content="<"/>`
+          }
+          <meta property="fc:frame:button:1:post_url" content="${
+            process.env.VERCEL_URL
+          }/frame?id=${idAsNumber + 1}"/>
+          <meta property="fc:frame:button:2" content=">"/>
+          <meta property="fc:frame:button:2:post_url" content="${
+            process.env.VERCEL_URL
+          }/frame?id=${idAsNumber - 1}"/>
+          </head>
+          </html>`
+    );
+  }
+};
+
+export const POST = async (req: NextRequest): Promise<Response> => {
+  return getResponse(req);
+};
+
+export const dynamic = "force-dynamic";

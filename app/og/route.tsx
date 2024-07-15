54ce009f3a89a01c84e7d5d324d13aa1bd4a17fa
MJ Baysic <mjpb123@gmail.com>
2024-07-15 17:29:30 -0400

    TEST

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -10,67 +10,14 @@ export async function GET(request: Request) {
 
     // ?title=<title>
     const hasTitle = searchParams.has("title");
-    const title = hasTitle
-      ? searchParams.get("title")?.slice(0, 100)
-      : "Default Title";
-
-    // ?description=
-    const hasDescription = searchParams.has("description");
-    const description = hasDescription
-      ? searchParams.get("description")?.slice(0, 100)
-      : "";
+    const title = hasTitle ? searchParams.get("title")?.slice(0, 100) : "Title";
 
     return new ImageResponse(
       (
-        <div
-          style={{
-            display: "flex",
-            height: "100%",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-            backgroundImage: "linear-gradient(to bottom, #dbf4ff, #fff1f1)",
-            fontSize: 80,
-            fontWeight: 700,
-            textAlign: "center",
-          }}
-        >
-          <p
-            style={{
-              backgroundImage:
-                "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))",
-              backgroundClip: "text",
-              color: "transparent",
-              fontSize: 80,
-              fontWeight: 700,
-              margin: 0,
-            }}
-          >
-            {title}
-          </p>
-          {description && (
-            <p
-              style={{
-                backgroundImage:
-                  "linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))",
-                backgroundClip: "text",
-                color: "transparent",
-                fontSize: 80,
-                fontWeight: 700,
-                margin: 0,
-                marginTop: 20,
-              }}
-            >
-              {description}
-            </p>
-          )}
+        <div className="h-full w-full flex items-center justify-center">
+          <p>{title}</p>
         </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      }
+      )
     );
   } catch (e: any) {
     console.log(`${e.message}`);

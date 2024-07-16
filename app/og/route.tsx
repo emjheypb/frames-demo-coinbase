f9975c1e6807aacd0d51e90ae55fc14420d1d4a1
MJ Baysic <mjpb123@gmail.com>
2024-07-16 11:19:46 -0400

    TEST

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -14,10 +14,39 @@ export async function GET(request: Request) {
 
     return new ImageResponse(
       (
-        <div className="h-full w-full flex items-center justify-center">
-          <p>{title}</p>
+        <div
+          style={{
+            display: "flex",
+            height: "100%",
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            backgroundImage: "linear-gradient(to bottom, #dbf4ff, #fff1f1)",
+            fontSize: 80,
+            fontWeight: 700,
+            textAlign: "center",
+          }}
+        >
+          <p
+            style={{
+              backgroundImage:
+                "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))",
+              backgroundClip: "text",
+              color: "transparent",
+              fontSize: 80,
+              fontWeight: 700,
+              margin: 0,
+            }}
+          >
+            {title}
+          </p>
         </div>
-      )
+      ),
+      {
+        width: 630,
+        height: 630,
+      }
     );
   } catch (e: any) {
     console.log(`${e.message}`);
